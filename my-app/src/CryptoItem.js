8f3aebcc0714a53132ba7ea849adec800253909d
chikini91 <khizhnyak.oleg91@gmail.com>
2018-08-13 20:01:39 +0300

    already finished

--- my-app/src/CryptoItem.js
+++ my-app/src/CryptoItem.js
@@ -6,15 +6,17 @@ class CryptoItem extends Component {
         super(props);
 
         this.state = {
-            checked: "checked",
-            data: {}
+            volume: 0,
+            price:{},
+            percent: {},
+            enablePercent: "checked"
         }
     }
 
 
     Check() {
         this.setState({
-            checked: !this.state.checked,
+            enablePercent: !this.state.enablePercent,
         })
     }
 
@@ -24,9 +26,14 @@ class CryptoItem extends Component {
 
         axios.get(apiUrl)
             .then(res => {
-                const data = res.data.changes;
 
-                this.setState({ data });
+                const percent = res.data.changes.percent;
+                const price = res.data.changes.price;
+                const volume = res.data.volume;
+
+                this.setState({ price });
+                this.setState({ percent });
+                this.setState({ volume });
             })
             .catch(err => console.log(err))
     }
@@ -38,15 +45,21 @@ class CryptoItem extends Component {
         axios.get(apiUrl)
             .then(res => {
 
-                const data = res.data.changes;
+                const percent = res.data.changes.percent;
+                const price = res.data.changes.price;
+                const volume = res.data.volume;
 
-                this.setState({ data });
+                this.setState({ price });
+                this.setState({ percent });
+                this.setState({ volume });
             })
             .catch(err => console.log(err))
     }
 
     render() {
-        console.log(this.state.data)
+
+        const {hour, day, week, mounth} = this.state.price || this.state.percent;
+        // const {hour, day, week, mounth} = this.state.percent;
 
         return (
             <li className="currency__crypto-item currency__crypto_eth">
@@ -55,7 +68,7 @@ class CryptoItem extends Component {
                 <div className="currency__crypto-info">
                     <div className="currency__crypto-info-price">
                         <span className="currency__crypto-info-price-text">Price:</span>
-                        <span className="currency__crypto-info-price-quantity">225 555</span>
+                        <span className="currency__crypto-info-price-quantity">${this.state.volume.toFixed(2)}</span>
                     </div>
                     <div className="currency__crypto-info-price">
                         <span>Percent change</span>
@@ -63,7 +76,7 @@ class CryptoItem extends Component {
                             <input type="checkbox"
                                    className="checkbox"
                                    id={`${this.props.name}-checkbox`}
-                                   checked={this.props.name === 'BTC' ? this.state.checked : !this.state.checked}
+                                   checked={this.props.name === 'BTC' ? this.state.enablePercent : !this.state.enablePercent}
                                    onChange={this.Check.bind(this)}
                             />
                             <label htmlFor={`${this.props.name}-checkbox`}/>
@@ -72,19 +85,19 @@ class CryptoItem extends Component {
                     <ul className="currency__crypto-info-period">
                         <li className="currency__crypto-info-period-item">
                             <span>Hour change</span>
-                            <span className="currency__crypto-info-value currency__crypto-info-value_negative">+555</span>
+                            <span className="currency__crypto-info-value">{this.state.enablePercent ? this.state.percent.hour : this.state.price.hour}</span>
                         </li>
                         <li className="currency__crypto-info-period-item">
                             <span>Day change</span>
-                            <span className="currency__crypto-info-value">+255</span>
+                            <span className={this.state.percent.day > 0 || this.state.price.day > 0 ? "currency__crypto-info-value" : "currency__crypto-info-value_negative"}>{this.state.enablePercent ? this.state.percent.day : this.state.price.day}</span>
                         </li>
                         <li className="currency__crypto-info-period-item">
                             <span>Week change</span>
-                            <span className="currency__crypto-info-value">+266%</span>
+                            <span className={this.state.percent.week > 0 || this.state.price.week > 0 ? "currency__crypto-info-value" : "currency__crypto-info-value_negative"}>{this.state.enablePercent ? this.state.percent.week : this.state.price.hour}</span>
                         </li>
                         <li className="currency__crypto-info-period-item">
                             <span>Mounth change</span>
-                            <span className="currency__crypto-info-value">+266%</span>
+                            <span className={this.state.percent.month > 0 || this.state.price.month > 0 ? "currency__crypto-info-value" : "currency__crypto-info-value_negative"}>{this.state.enablePecent ? this.state.percent.month : this.state.price.month}</span>
                         </li>
                     </ul>
                 </div>
